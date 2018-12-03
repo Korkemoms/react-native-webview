26f0dbc41ee545e4d8c3856a59d242e91f6ff7ab
andreas.modahl <andreas.modahl@websystemer.as>
2018-12-03 14:20:18 +0100

    bygg

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -449,7 +449,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
 
       }
 
-      public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+      public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
         new AlertDialog.Builder(reactContext).setTitle("Webtemp").setMessage(message)
             .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
               public void onClick(DialogInterface dialog, int which) {
